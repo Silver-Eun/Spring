ad4e139a56ae64d67d178804a18df5c13d79dd78
Silver-Eun <h22sang@naver.com>
2024-03-10 23:56:18 +0900

    Spring24
    AOP2

--- Hello-Spring/src/main/java/hello/HelloSpring/service/MemberService.java
+++ Hello-Spring/src/main/java/hello/HelloSpring/service/MemberService.java
@@ -17,18 +17,10 @@ public class MemberService {
 
     // 회원가입
     public long join(Member member) {
-        long start = System.currentTimeMillis();
-
-        try {
-            // 중복 회원 검증
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join " + timeMs + "ms");
-        }
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -39,15 +31,7 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
 }
