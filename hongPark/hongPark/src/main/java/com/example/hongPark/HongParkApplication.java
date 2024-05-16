77e20a1938b7484c5be6b69316fe07ce80aeb277
Silver-Eun <h22sang@naver.com>
2024-05-16 23:36:42 +0900

    Spring 1 - 2
    Output welcome username

---
+++ hongPark/hongPark/src/main/java/com/example/hongPark/HongParkApplication.java
@@ -0,0 +1,14 @@
+package com.example.hongPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongParkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HongParkApplication.class, args);
+	}
+
+}
---
+++ hongPark/hongPark/src/test/java/com/example/hongPark/HongParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
