27a4fc11777f7820462703dc9bb915531a69887c
Silver-Eun <h22sang@naver.com>
2024-06-03 21:26:24 +0900

    Spring 9 - 2
    show data list

--- hongPark/hongPark/src/main/java/com/example/hongPark/repository/ArticleRepository.java
+++ hongPark/hongPark/src/main/java/com/example/hongPark/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.hongPark.repository;
 import com.example.hongPark.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
