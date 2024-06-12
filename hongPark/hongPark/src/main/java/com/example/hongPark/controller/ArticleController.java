660d9823c6cffa058097f85cd4fd6ff1972503c9
Silver-Eun <h22sang@naver.com>
2024-06-12 17:38:12 +0900

    Spring 21 - 2
    comment list View

--- hongPark/hongPark/src/main/java/com/example/hongPark/controller/ArticleController.java
+++ hongPark/hongPark/src/main/java/com/example/hongPark/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.hongPark.controller;
 
 import com.example.hongPark.dto.ArticleForm;
+import com.example.hongPark.dto.CommentDto;
 import com.example.hongPark.entity.Article;
 import com.example.hongPark.repository.ArticleRepository;
+import com.example.hongPark.service.CommentService;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,8 @@ import java.util.List;
 public class ArticleController {
     @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 자동으로 연결
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -49,8 +53,10 @@ public class ArticleController {
 
         // 1. id로 데이터를 가져옴
         Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentDtos = commentService.comments(id);
         // 2. 가져온 데이터를 모델에 등록
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentDtos);
         // 3. 보여줄 페이지를 설정
         return "articles/show";
     }
