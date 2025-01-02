5c669470ebe1d3d68fbd0aaf274345f442990192
Silver-Eun <h22sang@naver.com>
2025-01-02 20:01:05 +0900

    songJava - 9
    array search

--- songJava/src/main/java/com/spring/songjava/mvc/parameter/BoardSearchParameter.java
+++ songJava/src/main/java/com/spring/songjava/mvc/parameter/BoardSearchParameter.java
@@ -1,11 +1,15 @@
 package com.spring.songjava.mvc.parameter;
 
+import com.spring.songjava.mvc.domain.BoardType;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class BoardSearchParameter {
 
     private String keyword;
+    private List<BoardType> boardTypes;
 
     public BoardSearchParameter() {
 
